1a9f9b5dd2f3a120f19446489e5d37a047ff6f18
Emir Şener <emir@cbot.ai>
2024-01-05 16:17:44 +0300

    build file değiştirildi

--- rebuild.sh
+++ rebuild.sh
@@ -23,7 +23,7 @@ echo "Rebuilding Jitsi-Meet web Docker image" && sleep 1
 # APPLE M1
 # docker build --platform linux/arm64/v8 -t jitsi/web . || { echo "Docker operation failed"; exit 1; }
 # LINUX SERVER
-docker build --platform linux/amd64 -t jitsi/web . || { echo "Docker operation failed"; exit 1; }
+docker build --platform linux/amd64 -t registry.cbot.ai/cbot-meet-ui . || { echo "Docker operation failed"; exit 1; }
 rm -rf $DOCKER_ROOT/web/jitsi-meet 
 
 echo "Taking down the Docker container stack" && sleep 1
